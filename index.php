c480d20757b3eb912e628301db818b553091e600
Igor Kobziev <igorok901@gmail.com>
2015-10-24 11:29:06 +0300

    commit

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
-
 <body>
 <div class="container-fluid">
 	<div class="wrap">
